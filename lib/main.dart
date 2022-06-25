e0f5521ba76b705b37d7f2f4e7b90ed408582f1c
Christo <georgievchristo@gmail.com>
2022-06-25 13:20:24 +0200

    Initial Firebase setup in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
@@ -11,6 +14,13 @@ import 'package:swole_app/constants/routes.dart';
 import 'package:swole_app/screens/add_workout/add_workout.dart';
 import 'package:swole_app/screens/home/home.dart';
 
+Future<void> _connectToFirebaseEmulator() async {
+  final localhost = Platform.isAndroid ? '10.0.2.2' : 'localhost';
+
+  FirebaseFirestore.instance.useFirestoreEmulator(localhost, 8080);
+  await FirebaseAuth.instance.useAuthEmulator(localhost, 9099);
+}
+
 void main() async {
   // https://docs.flutter.dev/testing/errors
   runZonedGuarded(() async {
@@ -19,6 +29,9 @@ void main() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    // Change to flavour based setup
+    // await _connectToFirebaseEmulator();
 
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitDown,
